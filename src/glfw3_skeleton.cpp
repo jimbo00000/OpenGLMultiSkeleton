b6307b8901e3736248c9412dc9c7b8e630fec98d
Jim Susinno <james.susinno@gmail.com>
2013-07-21 19:10:36 -0400

    Print monitor info in GLFW 3 skeleton.

--- src/glfw3_skeleton.cpp
+++ src/glfw3_skeleton.cpp
@@ -84,6 +84,69 @@ bool initGlfw(int argc, char **argv)
     return 1;
 }
 
+
+/// Dump a list of monitor info to Log and stdout.
+/// http://www.glfw.org/docs/3.0/monitor.html
+void PrintMonitorInfo()
+{
+    int count;
+    GLFWmonitor** monitors = glfwGetMonitors(&count);
+    printf("Found %d monitors:\n", count);
+    LOG_INFO("Found %d monitors:", count);
+    for (int i=0; i<count; ++i)
+    {
+        GLFWmonitor* pMonitor = monitors[i];
+        if (pMonitor == NULL)
+            continue;
+        printf("  Monitor %d:\n", i);
+        LOG_INFO("  Monitor %d:", i);
+
+        /// Monitor name
+        const char* pName = glfwGetMonitorName(pMonitor);
+        printf("    Name: %s\n", pName);
+        LOG_INFO("    Name: %s", pName);
+
+        /// Monitor Physical Size
+        int widthMM, heightMM;
+        glfwGetMonitorPhysicalSize(pMonitor, &widthMM, &heightMM);
+        //const double dpi = mode->width / (widthMM / 25.4);
+        printf("    physical size: %d x %d mm\n");
+        LOG_INFO("    physical size: %d x %d mm");
+
+        /// Modes
+        const GLFWvidmode* mode = glfwGetVideoMode(pMonitor);
+        printf("    Current mode:  %dx%d @ %dHz (RGB %d %d %d bits)\n",
+                mode->width,
+                mode->height,
+                mode->refreshRate,
+                mode->redBits,
+                mode->greenBits, 
+                mode->blueBits);
+        LOG_INFO("    Current mode:  %dx%d @ %dHz (RGB %d %d %d bits)",
+                mode->width,
+                mode->height,
+                mode->refreshRate,
+                mode->redBits,
+                mode->greenBits, 
+                mode->blueBits);
+
+#if 0
+        int modeCount;
+        const GLFWvidmode* modes = glfwGetVideoModes(pMonitor, &modeCount);
+        printf("    %d Modes:\n", modeCount);
+        LOG_INFO("    %d Modes:", modeCount);
+        for (int j=0; j<modeCount; ++j)
+        {
+            const GLFWvidmode& m = modes[j];
+            printf("      %dx%d @ %dHz (RGB %d %d %d bits)\n",
+                m.width, m.height, m.refreshRate, m.redBits, m.greenBits, m.blueBits);
+            LOG_INFO("      %dx%d @ %dHz (RGB %d %d %d bits)",
+                m.width, m.height, m.refreshRate, m.redBits, m.greenBits, m.blueBits);
+        }
+#endif
+    }
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Program main
 ////////////////////////////////////////////////////////////////////////////////
@@ -94,6 +157,8 @@ int main(int argc, char *argv[])
     if (!initGL(argc, argv))
         return 0;
     
+    PrintMonitorInfo();
+
     LOG_INFO("Starting main loop.");
 
     running = GL_TRUE;
